60c36321cf9660da5557f1392f3a1c3954a0d587
dzcazador <ctestaseca@hotmail.com>
2024-11-21 18:44:53 -0300

    Release Candidate v3

--- src/components/VentasDia.jsx
+++ src/components/VentasDia.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchVentas } from '../slices/ventasSlice';
-import { Table, TableHead, TableRow, TableCell, TableBody, Typography } from '@mui/material';
+import { Table, TableHead, TableRow, TableCell, TableBody, Typography, Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 import { format } from 'date-fns';
 
 const VentasDia = () => {
   const dispatch = useDispatch();
   const { items: ventas, status, error } = useSelector((state) => state.ventas);
+
+  const [openModal, setOpenModal] = useState(false);  // Estado para abrir/cerrar el modal
+  const [ventaSeleccionada, setVentaSeleccionada] = useState(null);  // Estado para la venta seleccionada para mostrar en el modal
+
 
   useEffect(() => {
     if (status === 'idle') {
@@ -42,6 +46,18 @@ const VentasDia = () => {
   // Filtrar las ventas para mostrar solo las de hoy
   const ventasDeHoy = ventas.filter(venta => getFechaSolo(venta.fechaHora) === getFechaHoy());
 
+    // Función para abrir el modal con los detalles de la venta
+    const handleAbrirModal = (venta) => {
+      setVentaSeleccionada(venta);
+      setOpenModal(true);
+    };
+  
+    // Función para cerrar el modal
+    const handleCerrarModal = () => {
+      setOpenModal(false);
+      setVentaSeleccionada(null);
+    };
+
 
   return (
     <div>
@@ -51,6 +67,7 @@ const VentasDia = () => {
           <TableRow>
             <TableCell>Fecha y Hora</TableCell>
             <TableCell>Importe Total</TableCell>
+            <TableCell>Detalle</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -58,10 +75,53 @@ const VentasDia = () => {
             <TableRow key={index}>
               <TableCell>{formatFecha(venta.fechaHora)}</TableCell>
               <TableCell>{parseFloat(venta.total).toFixed(2)}</TableCell> {/* Convertir total a número */}
+              <TableCell>
+                  <Button variant="outlined" onClick={() => handleAbrirModal(venta)}>
+                    Ver Detalle
+                  </Button>
+                </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
+
+      {/* Modal para mostrar el detalle de la venta */}
+      <Dialog open={openModal} onClose={handleCerrarModal}>
+        <DialogTitle>Detalle de la Venta</DialogTitle>
+        <DialogContent>
+          {ventaSeleccionada && (
+            <>
+              <Typography variant="h6">Fecha: {new Date(ventaSeleccionada.fechaHora).toLocaleString()}</Typography>
+              <Typography variant="h6">Items:</Typography>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Producto</TableCell>
+                    <TableCell>Cantidad</TableCell>
+                    <TableCell>Precio Unitario</TableCell>
+                    <TableCell>Total</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {ventaSeleccionada.items.map((item, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{item.nombreProducto}</TableCell>
+                      <TableCell>{item.cantidad}</TableCell>
+                      <TableCell>{item.precioUnitario}</TableCell>
+                      <TableCell>{(item.precioUnitario * item.cantidad)}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCerrarModal} color="primary">
+            Cerrar
+          </Button>
+        </DialogActions>
+      </Dialog>      
     </div>
   );
 };
